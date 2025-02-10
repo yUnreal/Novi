589b6c8dd1b5b2ce307c3913e196ac33b30f2e78
Unreal <unrealwebdev@gmail.com>
2025-02-10 16:13:50 -0300

    refactor: using const instead of namespace

--- src/index.ts
+++ src/index.ts
@@ -39,138 +39,96 @@ import type {
 	NoviSchemaOptions,
 } from './types/novi';
 
-export namespace n {
-	export type type<S extends NoviSchema> = S['type'];
+export * from './types/novi';
 
-	//#region Primitives
-	export const string = (options?: CoercibleSchemaOptions) =>
-		new NoviString(options);
-	export const number = (options?: CoercibleSchemaOptions) =>
-		new NoviNumber(options);
-	export const boolean = (options?: CoercibleSchemaOptions) =>
-		new NoviBoolean(options);
-	export const bigint = (options?: CoercibleSchemaOptions) =>
-		new NoviBigInt(options);
-	export const symbol = (options?: CoercibleSchemaOptions) =>
-		new NoviSymbol(options);
-
-	export const object = <Shape extends NoviObjectRecord>(
+export const n = {
+	string: (options?: CoercibleSchemaOptions) => new NoviString(options),
+	number: (options?: CoercibleSchemaOptions) => new NoviNumber(options),
+	boolean: (options?: CoercibleSchemaOptions) => new NoviBoolean(options),
+	bigint: (options?: CoercibleSchemaOptions) => new NoviBigInt(options),
+	symbol: (options?: CoercibleSchemaOptions) => new NoviSymbol(options),
+	object: <Shape extends NoviObjectRecord>(
 		shape: Shape,
 		options?: NoviSchemaOptions,
-	) => new NoviObject(shape, options);
-
-	export const file = (options?: NoviSchemaOptions) => new NoviFile(options);
-
-	export const date = (options?: CoercibleSchemaOptions) =>
-		new NoviDate(options);
-
-	export const set = <Value extends NoviSchema>(
-		value: Value,
-		options?: NoviSchemaOptions,
-	) => new NoviSet(value, options);
-	export const map = <Key extends NoviSchema, Value extends NoviSchema>(
+	) => new NoviObject(shape, options),
+	file: (options?: NoviSchemaOptions) => new NoviFile(options),
+	date: (options?: CoercibleSchemaOptions) => new NoviDate(options),
+	set: <Value extends NoviSchema>(value: Value, options?: NoviSchemaOptions) =>
+		new NoviSet(value, options),
+	map: <Key extends NoviSchema, Value extends NoviSchema>(
 		key: Key,
 		value: Value,
 		options?: NoviSchemaOptions,
-	) => new NoviMap(key, value, options);
-
-	export const array = <Item extends NoviSchema>(
-		item: Item,
+	) => new NoviMap(key, value, options),
+	array: <Item extends NoviSchema>(item: Item, options?: NoviSchemaOptions) =>
+		new NoviArray(item, options),
+	func: <Return extends NoviSchema, const Args extends NoviSchema[]>(
 		options?: NoviSchemaOptions,
-	) => new NoviArray(item, options);
-
-	export const func = <
-		Return extends NoviSchema,
-		const Args extends NoviSchema[],
-	>(
-		options?: NoviSchemaOptions,
-	) => new NoviFunction<Return, Args>(options);
-	//#endregion
-
-	//#region Customs
-	export const tuple = <const Items extends NoviSchema[]>(
+	) => new NoviFunction<Return, Args>(options),
+	tuple: <const Items extends NoviSchema[]>(
 		items: Items,
 		options?: NoviSchemaOptions,
-	) => new NoviTuple(items, options);
-	export const unknown = (options?: NoviSchemaOptions) =>
-		new NoviUnknown(options);
-
-	export const record = <Key extends NoviPropertyKey, Value extends NoviSchema>(
+	) => new NoviTuple(items, options),
+	unknown: (options?: NoviSchemaOptions) => new NoviUnknown(options),
+	record: <Key extends NoviPropertyKey, Value extends NoviSchema>(
 		key: Key,
 		value: Value,
 		options?: NoviSchemaOptions,
-	) => new NoviRecord(key, value, options);
-
-	export const literal = <const Value>(
-		value: Value,
-		options?: NoviSchemaOptions,
-	) => new NoviLiteral(value, options);
-
-	export const instance = <Instance extends Constructor>(
+	) => new NoviRecord(key, value, options),
+	literal: <const Value>(value: Value, options?: NoviSchemaOptions) =>
+		new NoviLiteral(value, options),
+	instance: <Instance extends Constructor>(
 		instance: Instance,
 		options?: NoviSchemaOptions,
-	) => new NoviInstance(instance, options);
-
-	export const never = (options?: NoviSchemaOptions) => new NoviNever(options);
-
-	export const nullable = <Schema extends NoviSchema>(
+	) => new NoviInstance(instance, options),
+	never: (options?: NoviSchemaOptions) => new NoviNever(options),
+	nullable: <Schema extends NoviSchema>(
 		schema: Schema,
 		options?: NoviSchemaOptions,
-	) => new NoviNullable(schema, options);
-
-	export const optional = <Schema extends NoviSchema>(
+	) => new NoviNullable(schema, options),
+	optional: <Schema extends NoviSchema>(
 		schema: Schema,
 		options?: NoviSchemaOptions,
-	) => new NoviOptional(schema, options);
-
-	export const promise = <Schema extends NoviSchema>(
+	) => new NoviOptional(schema, options),
+	promise: <Schema extends NoviSchema>(
 		schema: Schema,
 		options?: NoviSchemaOptions,
-	) => new NoviPromise(schema, options);
-
-	export const nativeEnum = <Enum extends NoviEnumShape>(
+	) => new NoviPromise(schema, options),
+	enum: <Enum extends NoviEnumShape>(
 		shape: Enum,
 		options?: NoviSchemaOptions,
-	) => new NoviEnum(shape, options);
-
-	export const or = <Unions extends NoviSchema[]>(
+	) => new NoviEnum(shape, options),
+	or: <Unions extends NoviSchema[]>(
 		unions: Unions,
 		options?: NoviSchemaOptions,
-	) => new NoviUnion(unions, options);
-	export const and = <Intersections extends NoviSchema[]>(
+	) => new NoviUnion(unions, options),
+	and: <Intersections extends NoviSchema[]>(
 		intersections: Intersections,
 		options?: NoviSchemaOptions,
-	) => new NoviIntersection(intersections, options);
-
-	export const jwt = <Schema extends NoviObject<NoviObjectRecord>>(
+	) => new NoviIntersection(intersections, options),
+	jwt: <Schema extends NoviObject<NoviObjectRecord>>(
 		schema: Schema,
 		options?: NoviSchemaOptions,
-	) => new NoviJWT(schema, options);
-
-	export const nullish = <Schema extends NoviSchema>(
+	) => new NoviJWT(schema, options),
+	nullish: <Schema extends NoviSchema>(
 		schema: Schema,
 		options?: NoviSchemaOptions,
-	) => new NoviNullish(schema, options);
-
-	export const readonly = <Schema extends NoviSchema>(
+	) => new NoviNullish(schema, options),
+	readonly: <Schema extends NoviSchema>(
 		schema: Schema,
 		options?: NoviSchemaOptions,
-	) => new NoviReadonly(schema, options);
-
-	export const custom = <Type>(
+	) => new NoviReadonly(schema, options),
+	custom: <Type>(
 		fun?: (value: unknown) => boolean,
 		options?: NoviCustomSchemaOptions,
-	) => new NoviCustom<Type>(fun, options);
-
-	export const _catch = <Schema extends NoviSchema>(
+	) => new NoviCustom<Type>(fun, options),
+	catch: <Schema extends NoviSchema>(
 		fun: () => Schema['type'],
 		schema: Schema,
 		options?: NoviSchemaOptions,
-	) => new NoviCatch(fun, schema, options);
-
-	export const lazy = <Schema extends NoviSchema>(
+	) => new NoviCatch(fun, schema, options),
+	lazy: <Schema extends NoviSchema>(
 		fun: () => Schema,
 		options?: NoviSchemaOptions,
-	) => new NoviLazy(fun, options);
-	//#endregion
-}
+	) => new NoviLazy(fun, options),
+};
